2be8ea81739c210505ae7d992ccb9cdf0c9b430a
dema24 <113282370+dema24@users.noreply.github.com>
2022-10-24 07:23:12 +0000

    [Start]応用課題３

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,6 @@
 class BooksController < ApplicationController
+   before_action :ensure_correct_user, only: [:edit, :update, :destroy]
+
 
   def show
     @book = Book.find(params[:id])
@@ -23,12 +25,10 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
-    is_matching_login_user
   end
 
   def update
     @book = Book.find(params[:id])
-    is_matching_login_user
     if @book.update(book_params)
       redirect_to book_path(@book), notice: "You have updated book successfully."
     else
@@ -48,8 +48,9 @@ class BooksController < ApplicationController
     params.require(:book).permit(:title, :body)
   end
 
-  def is_matching_login_user
-    if @book.user != current_user
+  def ensure_correct_user
+    @book = Book.find(params[:id])
+    unless @book.user == current_user
       redirect_to books_path
     end
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :ensure_correct_user, only: [:update]
+  before_action :ensure_correct_user, only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -14,12 +14,10 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
-    is_matching_login_user
   end
 
   def update
     @user = User.find(params[:id])
-    is_matching_login_user
     if @user.update(user_params)
       redirect_to user_path(current_user), notice: "You have updated user successfully."
     else
@@ -40,9 +38,4 @@ class UsersController < ApplicationController
     end
   end
 
-  def is_matching_login_user
-    if @user != current_user
-      redirect_to user_path(current_user)
-    end
-  end
 end
