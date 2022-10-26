f68b28806785cb1e3355b3eae2c61f5e4c11d57a
dema24 <113282370+dema24@users.noreply.github.com>
2022-10-26 13:20:20 +0000

    [Finish]応用課題５

--- app/models/user.rb
+++ app/models/user.rb
@@ -38,4 +38,18 @@ class User < ApplicationRecord
     followings.include?(user)
   end
 
+  def self.looks(search, word)
+    if search == "perfect_match"
+      @user = User.where("name LIKE?", "#{word}")
+    elsif search == "forword_match"
+      @user = User.where("name LIKE?", "%#{word}")
+    elsif search == "backword_match"
+      @user = User.where("name LIKE?", "#{word}%")
+    elsif search == "partial_match"
+      @user = User.where("name LIKE?", "%#{word}%")
+    else
+      @users = User.all
+    end
+  end
+
 end
