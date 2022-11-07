f03e518d6a9a8f2453201f7de5bb47ae815a7fdc
dema24 <113282370+dema24@users.noreply.github.com>
2022-11-07 13:25:17 +0000

    [Finish]応用課題7c

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -20,12 +20,21 @@ class GroupsController < ApplicationController
   end
 
   def show
+    @group = Group.find(params[:id])
+    @book =Book.new
   end
 
   def edit
+    @group = Group.find(params[:id])
   end
 
   def update
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+      redirect_to groups_path
+    else
+      render "edit"
+    end
   end
 
   def group_params
