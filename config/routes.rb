aafe8cf75d9fc66f49c5933e86bfe8bfae08ffa7
dema24 <113282370+dema24@users.noreply.github.com>
2022-11-07 13:52:27 +0000

    [Start]応用課題8c

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,8 @@ Rails.application.routes.draw do
 
   resources :chats, onry: [:show, :create]
 
-  resources :groups, only: [:index, :show, :edit, :new, :create, :update]
+  resources :groups do
+    get "join" => "groups#join", as: "join"
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
  end
